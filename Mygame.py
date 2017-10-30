9f8de264189aab7354632967d2740181dd62f0fe
Dohyun <eheh5454@gmail.com>
2017-10-30 16:22:40 +0900

    배경화면, 군인 구현

---
+++ Mygame.py
@@ -0,0 +1,8 @@
+from pico2d import *
+
+import Stage1
+import game_framework
+
+open_canvas()
+game_framework.run(Stage1)
+close_canvas()
---
+++ Stage1.py
@@ -0,0 +1,67 @@
+from pico2d import *
+
+import game_framework
+from Resource import *
+
+space = None
+soldier = None
+
+
+class Space:
+    def __init__(self):
+        self.image = load_image('Space.jpg')
+
+    def draw(self):
+        self.image.draw(400, 300)
+
+
+class Soldier:
+    def __init__(self):
+        self.x, self.y = 400, 300
+        self.frame = 0
+        self.image = load_image('soldier.png')
+
+    def update(self):
+        self.frame = (self.frame + 1) % 8
+        delay(0.05)
+
+    def draw(self):
+        self.image.clip_draw(self.frame*50, 0, 50, 90, self.x, self.y)
+
+
+def enter():
+    global space, soldier
+    space = Space()
+    soldier = Soldier()
+
+
+def exit():
+    global space, soldier
+    del(space)
+    del(soldier)
+
+
+def pause():
+    pass
+
+
+def resume():
+    pass
+
+
+def handle_events():
+    events = get_events()
+    for event in events:
+        if event.type == SDL_KEYDOWN and event.key == SDLK_ESCAPE:
+            game_framework.quit()
+
+
+def update():
+    soldier.update()
+
+
+def draw():
+    clear_canvas()
+    space.draw()
+    soldier.draw()
+    update_canvas()
