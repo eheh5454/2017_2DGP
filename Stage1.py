7e3cf1553a044b43023c5a252dcaaba3061f3168
Dohyun <eheh5454@gmail.com>
2017-11-01 23:16:53 +0900

    make Eye_monster

--- Stage1.py
+++ Stage1.py
@@ -29,17 +29,48 @@ class Soldier:
         self.image.clip_draw(self.frame*50, 0, 50, 90, self.x, self.y)
 
 
+class Eye_monster:
+    def __init__(self):
+        self.x, self.y = 50, 50
+        self.frame = 0
+        self.image = load_image('Eye_monster.png')
+        self.xmove = 10
+        self.ymove = 10
+
+    def update(self):
+        self.frame = (self.frame + 1) % 7
+        self.move()
+        delay(0.05)
+
+
+    def draw(self):
+        self.image.clip_draw(self.frame*70, 0, 70, 70, self.x, self.y)
+
+    def move(self):
+        self.x += self.xmove
+        self.y += self.ymove
+        if self.x > 800:
+            self.xmove = -self.xmove
+        if self.x < 0:
+            self.xmove = -self.xmove
+        if self.y > 600:
+            self.ymove = -self.ymove
+        if self.y < 0:
+            self.ymove = -self.ymove
+
 
 def enter():
-    global space, soldier
+    global space, soldier, eye_monster
     space = Space()
     soldier = Soldier()
+    eye_monster = Eye_monster()
 
 
 def exit():
-    global space, soldier
+    global space, soldier, eye_monster
     del(space)
     del(soldier)
+    del(eye_monster)
 
 
 def pause():
@@ -67,10 +98,12 @@ def handle_events():
 
 def update():
     soldier.update()
+    eye_monster.update()
 
 
 def draw():
     clear_canvas()
     space.draw()
     soldier.draw()
+    eye_monster.draw()
     update_canvas()
