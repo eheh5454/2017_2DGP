31376dcb9b88f29d3955828514bb6aab2939a229
Dohyun <eheh5454@gmail.com>
2017-12-18 23:06:53 +0900

    restart 추가

--- Game_over.py
+++ Game_over.py
@@ -1,25 +1,27 @@
 from pico2d import *
 import game_framework
 import Stage1
-import Title
+
 
 image = None
 space = None
-font = None
-
+score_font = None
+restart_font = None
 
 def enter():
-    global image, space, font
+    global image, space, score_font, restart_font
     image = load_image('game_over.png')
     space = load_image('Space.jpg')
-    font = load_font('ENCR10B.TTF', 50)
+    score_font = load_font('ENCR10B.TTF', 50)
+    restart_font = load_font('YGD360.TTF', 70)
 
 
 def exit():
-    global image, space, font
+    global image, space, score_font, restart_font
     del image
     del space
-    del font
+    del score_font
+    del restart_font
 
 
 def update(frame_time):
@@ -27,11 +29,12 @@ def update(frame_time):
 
 
 def draw(frame_time):
-    global image, space, Score
+    global image, space, Score, restart_font
     clear_canvas()
     space.draw(400, 300)
     image.draw(400, 300)
-    font.draw(200, 400, 'Your Score:%d' % Stage1.Score, (220, 220, 0))
+    score_font.draw(200, 400, 'Your Score:%d' % Stage1.Score, (220, 220, 0))
+    restart_font.draw(50, 100, 'SPACE TO RESTART', (250, 250, 0))
     update_canvas()
 
 
@@ -40,7 +43,7 @@ def handle_events(frame_time):
     for event in events:
             if (event.type, event.key) == (SDL_KEYDOWN, SDLK_ESCAPE):
                 game_framework.quit()
-            elif (event.type, event.key) == (SDL_KEYDOWN, SDLK_p):
+            elif (event.type, event.key) == (SDL_KEYDOWN, SDLK_SPACE):
                 game_framework.pop_state()
 
 
